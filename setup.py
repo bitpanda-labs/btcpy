6b3bf768c7fc76658f74d8493451b61b83063d2b
ilker.icyuz <ilker.icyuz@bitpanda.com>
2022-08-17 13:38:22 +0200

    changes from 0.2.81

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='chainside-btcpy-multi',
-      version='0.2.80',
+      version='0.2.81',
       packages=find_packages(),
       install_requires=['ecdsa>=0.15.0,<0.16.0'],
       extras_require={'develop': ['python-bitcoinlib==0.7.0']},
